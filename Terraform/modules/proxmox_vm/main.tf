fa79138f6eaf9854de59cc64c32c3e9761594324
Rami-hardware <killx51@hotmail.com>
2026-02-12 13:11:25 +0300

    test

--- Terraform/modules/proxmox_vm/main.tf
+++ Terraform/modules/proxmox_vm/main.tf
@@ -44,6 +44,17 @@ dynamic "disk" {
     }
   }
 
+dynamic "hostpci" {
+  for_each = var.hostpcis
+  content {
+    device = hostpci.value
+    # optional settings:
+    # pci_model = "vfio-pci"
+    # romfile   = "auto"
+    # iommu     = true
+  }
+}
+
   network_device {
     bridge = "vmbr0"
   }
