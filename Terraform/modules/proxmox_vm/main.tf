42e86dcff39eb3b215f7028e9084138107d760c2
Rami-hardware <killx51@hotmail.com>
2026-04-17 05:49:18 +0300

    goo

--- Terraform/modules/proxmox_vm/main.tf
+++ Terraform/modules/proxmox_vm/main.tf
@@ -62,6 +62,21 @@ resource "proxmox_virtual_environment_vm" "vm" {
       clone,
       hostpci,
       cpu,
+      memory,
+      agent,
+      operating_system,
+      migrate,
+      on_boot,
+      reboot,
+      stop_on_destroy,
+      timeout_clone,
+      timeout_create,
+      timeout_migrate,
+      timeout_move_disk,
+      timeout_reboot,
+      timeout_shutdown_vm,
+      timeout_start_vm,
+      timeout_stop_vm,
     ]
   }
 
